c70ed9d9ed911ec0d741b6da2e2e42eb51d232fb
Jon Daniel <joneqdaniel@gmail.com>
2025-09-07 15:49:12 +0200

    add instal shell script and update join.sh

---
+++ M3D_LIBS/install_M3D.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+[[ -z "${PSPDEV}" ]] && [[ -z "${@}" ]] && echo "Error: PSPDEV variable not set and/or no arguments given!" && exit
+
+if [[ ! -z "${@}" ]] then
+PSPSDKDIR=$@/psp/sdk
+else
+PSPSDKDIR=$PSPDEV/psp/sdk
+fi
+
+echo -n "Creating target dir: ${PSPSDKDIR}"
+mkdir -p $PSPSDKDIR || exit " Failure!"
+echo " Successful!"
+
+echo -n "Creating release dir..."
+mkdir -p release/include release/lib || exit " Failure!"
+echo " Successful!"
+
+cp -ar AMGLib/M3D.h release/include
+cp -ar AMGLib/libAMG.a release/lib
+cp -ar bullet-2.82-r2704/src/libbulletpsp.a release/lib
+cp -ar oslibmodv2-master/libosl.a release/lib
+
+cd release/lib && ./join.sh && cd ../..
+
+echo -n "Installing libs..."
+install -t $PSPSDKDIR/lib/ release/lib/libM3D.a && \
+install -t $PSPSDKDIR/include/ release/include/M3D.h || exit " Failure!"
+echo " Successful!"
